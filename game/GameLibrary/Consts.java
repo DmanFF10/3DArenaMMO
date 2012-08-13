345250da288bfc850cc5044952e88d1d122b6e82
Benjamin Pratt <Gauger16@gmail.com>
2012-08-13 12:15:25 -0700

    worked on the camera positioning the camera now stays at a set position around the players object. player object now rotates with the camera. movment when the camera rotates has some bugs. also canera jumps around a bit when going diagnaly.
    This reverts commit 2bf8d830f95df2d9fa1b04b213777e86173b231b.

--- game/GameLibrary/Consts.java
+++ game/GameLibrary/Consts.java
@@ -11,7 +11,6 @@ public class Consts {
 	public static final int TYPE_MOVE = 1;
 	public static final int TYPE_MESSAGE = 2;
 	public static final int TYPE_NEW_PlAYER = 3;
-	public static final int TYPE_LOGOUT = 4;
 	
 	public static final int MESSAGE_ONE = 0;
 	public static final int MESSAGE_MANY = 1;
@@ -20,6 +19,4 @@ public class Consts {
 	public static final int MOVE_STOP = 0;
 	public static final int MOVE_FORWORD_LEFT = -1;
 	public static final int MOVE_BACKWORD_RIGHT = 1;
-	
-	public static final float UNITSIZE = 0.0002f;
 }
